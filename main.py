87f5c86f5c6c1e6c4e918fc570a40af9d0af6713
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-03-07 18:55:10 +0000

    made the graphs but lines in the way

--- main.py
+++ main.py
@@ -5,12 +5,45 @@ import turtle
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+import math
+wn=turtle.Screen()
+fred = turtle.Turtle()
+turtle.setworldcoordinates(-360,-1.5,360,1.5)
+
+def drawSineCurve(myturtle=None):
+  for angle in range(-360, 360):
+    y = math.sin(math.radians(angle))
+    fred.goto(angle, y)
+
+def setupWindow(mywindow=None):
+  wn = turtle.Screen()
+  turtle.bgcolor("lightgreen")
+  turtle.setworldcoordinates(-360, -1.5, 3, 60, 1.5)
+
+def setupAxis(myturtle=None):
+  fred.goto(-360, 0)
+  fred.goto(360, 0)
+  fred.goto(0,0)
+  fred.goto(0, -1.5)
+  fred.goto (0, 1.5)
+  fred.goto(0,0)
+
+def drawCosineCurve(myturtle=None):
+  for angle in range(-360,361):
+    y=math.cos(math.radians(angle))
+    fred.goto(angle, y)
+
+def drawTangentCurve(myturtle=None):
+  fred.speed(0)
+  for angle in range(-360, 361):
+    y=math.tan(math.radians(angle))
+    fred.goto(angle,y)
 
 
-
-
-
-
+setupAxis()
+drawSineCurve()
+drawCosineCurve()
+drawTangentCurve()
 
 
 ##########  Do Not Alter Any Code Past Here ########
